36a8ecef98301e7e0f032a05f15d63ae557634bd
Alex-prog814 <aleks.98.x.28@gmail.com>
2023-02-09 12:22:39 +0600

    Added first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Header from './components/Header/Header';
+import AddProduct from './components/AddProduct/AddProduct';
+import ProductList from './components/ProductList/ProductList';
+import EditProduct from './components/EditProduct/EditProduct';
 
-function App() {
+const App = () => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [showEditModal, setShowEditModal] = useState(false);
+  const handleEditClose = () => setShowEditModal(false);
+  const handleEditShow = () => setShowEditModal(true);
+
+  const [products, setProducts] = useState([]);
+
+  const [editedProduct, setEditedProduct] = useState({});
+
+  function addProduct(newObj) {
+    let newProducts = [...products];
+    newProducts.push(newObj);
+    setProducts(newProducts);
+
+    // setProducts([...products, newObj]);
+  };
+
+  function getOneProduct(id) {
+    let obj = products.find(item => item.id === id);
+    setEditedProduct(obj);
+    setShowEditModal(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header handleShow={handleShow} />
+      <AddProduct show={show} handleClose={handleClose} addProduct={addProduct} />
+      <ProductList products={products} getOneProduct={getOneProduct} />
+      {showEditModal && <EditProduct showEditModal={showEditModal} handleEditClose={handleEditClose} editedProduct={editedProduct} />}
+    </>
+  )
 }
 
 export default App;
